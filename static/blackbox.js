75654deda375cb4e2af33d820fa938553acd5ab8
rtty69420 <132277226+rtty69420@users.noreply.github.com>
2024-08-16 15:41:03 +0000

    committed

---
+++ static/blackbox.js
@@ -0,0 +1,12 @@
+const ai = require("@ozrageharm/blackbox-ai");
+
+const form = document.getElementsByName('formm');
+const input = document.getElementsByName('ai');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    const output = ai.chat(input.value.trim());
+    const result = document.querySelector(".output");
+
+    result.innerText = input.value.trim();
+});
